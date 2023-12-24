fa25ac7b80d4f05f31307ce1cf5740ef744ed5d4
Otabek <mavlonovo@tamu.edu>
2023-12-23 23:04:55 -0600

    added GET handler

--- 5.6 Secrets Project/index.js
+++ 5.6 Secrets Project/index.js
@@ -1,12 +1,24 @@
 // HINTS:
 // 1. Import express and axios
+import express from "express";
+import axios from "axios";
 
 // 2. Create an express app and set the port number.
-
+const app = express();
+const port = 3000;
 // 3. Use the public folder for static files.
+app.use(express.static("public"));
 
 // 4. When the user goes to the home page it should render the index.ejs file.
+app.get("/", async (req, res) => {
 
+  const respone = await axios.get("");
+  try {
+    res.render("index.ejs");
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+});
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
 
